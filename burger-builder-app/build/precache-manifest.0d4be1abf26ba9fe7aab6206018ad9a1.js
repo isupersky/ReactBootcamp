fe0a3e63f099bf3816bd59e2b35f8a5cd4037dca
Aakash Sinha <60641108+iAakashSinha@users.noreply.github.com>
2020-05-11 21:04:05 +0530

    React_Routing final

---
+++ burger-builder-app/build/precache-manifest.0d4be1abf26ba9fe7aab6206018ad9a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a6f46b5745a1e5ba708e71f364bd736",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d49c974e451ae5c53f28",
+    "url": "/static/css/main.24e955ec.chunk.css"
+  },
+  {
+    "revision": "00288a232d0aa97fd652",
+    "url": "/static/js/2.305f8d25.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.305f8d25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d49c974e451ae5c53f28",
+    "url": "/static/js/main.a951e1a0.chunk.js"
+  },
+  {
+    "revision": "edfd378b59faa8d226e5",
+    "url": "/static/js/runtime-main.f6cf3941.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
