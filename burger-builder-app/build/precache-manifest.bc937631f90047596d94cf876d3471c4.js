234dcf41c615ead5529d07fcae65141e899d5af2
Aakash Sinha <60641108+iAakashSinha@users.noreply.github.com>
2020-05-11 11:14:30 +0530

    Interacting_with_server final

---
+++ burger-builder-app/build/precache-manifest.bc937631f90047596d94cf876d3471c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9190be0bc6897ab444913495d7e212b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "765d4e20ba4609d8d1dd",
+    "url": "/static/css/main.77bf9964.chunk.css"
+  },
+  {
+    "revision": "02a2b8f51626d3ab7e5a",
+    "url": "/static/js/2.21e603f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.21e603f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "765d4e20ba4609d8d1dd",
+    "url": "/static/js/main.91390a63.chunk.js"
+  },
+  {
+    "revision": "edfd378b59faa8d226e5",
+    "url": "/static/js/runtime-main.f6cf3941.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
