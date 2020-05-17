ea118f56ba6fcedd6ebc9e304eb006b4fe60fe8d
Aakash Sinha <60641108+iAakashSinha@users.noreply.github.com>
2020-05-17 11:40:49 +0530

    React-Authentication final

---
+++ burger-builder-app/build/precache-manifest.29e1d2c9137d8c52ddcb3831e0c76a2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbae2e9d6cc3e5468d940d3d18890601",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a260a1ceba553d05641",
+    "url": "/static/css/main.e261abba.chunk.css"
+  },
+  {
+    "revision": "b8862e96881c5990c960",
+    "url": "/static/js/2.e655ccc8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e655ccc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a260a1ceba553d05641",
+    "url": "/static/js/main.8f2131ab.chunk.js"
+  },
+  {
+    "revision": "edfd378b59faa8d226e5",
+    "url": "/static/js/runtime-main.f6cf3941.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
